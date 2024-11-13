e03c7bcae29f2a3402689b83ded56554183bd64d
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-13 15:25:22 +0100

    add footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,10 +1,7 @@
 export default function AppFooter() {
 	return (
 		<footer className=' bg-dark text-center py-3'>
-			<p className='text-white m-0'>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusantium suscipit non ipsa magni harum quam.
-				Magnam recusandae vitae perspiciatis, ducimus ab laudantium animi facilis!{' '}
-			</p>
+			<p className='text-white m-0'>Made with ❤️ by Boolean</p>
 		</footer>
 	)
 }
