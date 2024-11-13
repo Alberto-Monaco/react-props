25c10f40c8440e8e0a7d3b5056bc981ee3bd2c2d
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-13 14:49:43 +0100

    add published if

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -5,7 +5,7 @@ export default function AppMain() {
 	return (
 		<main className='bg-light py-4 '>
 			<div className='container'>
-				<div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 justify-content-center'>
+				<div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 justify-content-center'>
 					{posts.map((post) => (
 						<div className='col' key={post.id}>
 							<Card post={post} />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,17 @@
 
 export default function Card({ post }) {
 	return (
-		<div className='card border-1 border-light-subtle shadow'>
-			<img src={post.image} alt='card-img' className='card-img rounded-top rounded-0' />
-			<div className='card-body'>
-				<h5 className='card-title'>{post.title}</h5>
-				<p className='card-text'>{post.content}</p>
-				<p className='card-text'>{post.tags.join(', ')}</p>
-			</div>
-		</div>
+		<>
+			{post.published && (
+				<div className='card border-1 border-light-subtle shadow'>
+					<img src={post.image} alt='card-img' className='card-img rounded-top rounded-0' />
+					<div className='card-body'>
+						<h5 className='card-title'>{post.title}</h5>
+						<p className='card-text'>{post.content}</p>
+						<p className='card-text'>{post.tags.join(', ')}</p>
+					</div>
+				</div>
+			)}
+		</>
 	)
 }
